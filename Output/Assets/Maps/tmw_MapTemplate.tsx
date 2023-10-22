a7c48e1f6bd43ab76d54a28a0889ee5a66414fdf
Rebeca <rebeca.fernandez@estudiantat.upc.edu>
2023-10-22 18:08:31 +0200

    Map assets+ tmx to fix

---
+++ Output/Assets/Maps/tmw_MapTemplate.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tmw_plataforms_sky" tilewidth="16" tileheight="16" tilecount="1332" columns="36">
+ <image source="tmw_plataforms_sky.png" width="576" height="606"/>
+</tileset>
